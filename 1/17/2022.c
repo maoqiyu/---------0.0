128b8d3055dd966a0a8724608f41fa3c997d8f48
maoqiyu <583543058@qq.com>
2022-01-18 19:48:17 +0800

    Update 2022.c

--- 1/17/2022.c
+++ 1/17/2022.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#if 0
 int Swap(int* a, int* b)//交换两个整数，练习函数调用，使用指针
 {
     int t;
@@ -15,3 +16,30 @@ int main(){
     printf("%d \t %d",a,b);
     return 0;
 }
+#endif
+#if 1
+int main(){
+    int a,b;
+    char operater;//+-*/运算符
+    printf("请输入");//整数 运算符 整数
+    scanf("%d %c %d",&a,&operater,&b);
+    switch (operater)
+    {
+    case ('+'):
+       return printf("%d",a+b);
+        break;
+    case ('-'):
+       return printf("%d",a-b);
+        break;
+    case ('*'):
+        return printf("%d",a*b);
+        break;
+    case ('/'):
+       return printf("%d",a/b);
+        break;
+    default:
+       return printf("输入的运算符不属于加减乘除");
+        break;
+    }
+}
+#endif
